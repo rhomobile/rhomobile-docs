adb4f93ae935f03cda2b7cf26bdb8913cc132774
Ryan Tomayko <rtomayko@gmail.com>
2009-07-20 19:04:49 -0700

    disable search when CLOUDQUERY_USERNAME not defined

--- search.rb
+++ search.rb
@@ -1,44 +1,49 @@
 require 'cloudquery'
 
-SEARCH = (
-	secret = Cloudquery::Client.get_secret(
-		ENV["CLOUDQUERY_USERNAME"],
-		ENV["CLOUDQUERY_PASSWORD"]
+if ENV['CLOUDQUERY_USERNAME']
+
+	SEARCH = (
+		secret = Cloudquery::Client.get_secret(
+			ENV["CLOUDQUERY_USERNAME"],
+			ENV["CLOUDQUERY_PASSWORD"]
+		)
+
+		Cloudquery::Client.new(
+			:account => ENV["CLOUDQUERY_USERNAME"],
+			:secret => secret
+		)
 	)
 
-	Cloudquery::Client.new(
-		:account => ENV["CLOUDQUERY_USERNAME"],
-		:secret => secret
+	SEARCH.add_indexes("heroku.docs")
+
+	SEARCH.add_schema(<<-XML)
+	<schema name="heroku.docs.page" store="yes">
+		<field
+			name="name"
+			type="string"
+			usage="user"
+		/>
+		<field
+			name="content"
+			type="string"
+			usage="user"
+		/>
+	</schema>
+	XML
+
+	SEARCH.update_documents(
+		"heroku.docs",
+		(
+			Dir["docs/*.txt"].map do |section|
+				{
+					"#.#" => name = File.basename(section, ".txt"),
+					"name" => name,
+					"content" => File.read(section)
+				}
+			end
+		),
+		"heroku.docs.page"
 	)
-)
-
-SEARCH.add_indexes("heroku.docs")
-
-SEARCH.add_schema(<<-XML)
-<schema name="heroku.docs.page" store="yes">
-	<field
-		name="name"
-		type="string"
-		usage="user"
-	/>
-	<field
-		name="content"
-		type="string"
-		usage="user"
-	/>
-</schema>
-XML
-
-SEARCH.update_documents(
-	"heroku.docs",
-	(
-		Dir["docs/*.txt"].map do |section|
-			{
-				"#.#" => name = File.basename(section, ".txt"),
-				"name" => name,
-				"content" => File.read(section)
-			}
-		end
-	),
-	"heroku.docs.page"
-)
+else
+	warn "CLOUDQUERY_USERNAME environment not defined. search disabled"
+end
