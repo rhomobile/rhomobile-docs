c0b45188fde63ca865652334dc3b113d186f335c
oren <oren@heroku.com>
2009-07-23 21:53:07 -0700

    Added a backlog explanaition, taken straight from a post from Adam Added custom domain text explaining host to use a records Large reorder of the TOC.  Marked any blanks with XX

--- toc.rb
+++ toc.rb
@@ -1,34 +1,53 @@
 section 'getting-started', "Getting Started" do
+ topic 'a', 'What is Heroku? XX'
+ topic 'b', 'First application walkthrough XX'
+ topic 'quickstart',      'Advanced user quickstart'
+ topic 'z', "What is a dyno? XX" 
+ topic 'technologies',    'Installed software'
+
 
 end
 
 section 'lifecycle', "App Lifecycle" do
+  topic 'd', 'Initial deployment XX'
+  topic 'g', 'Migrating your application XX'
+  topic 'e', 'Staging & Production XX'
+  topic 'f', 'Scaling for success XX'
+  topic 'h', 'Keeping everything running smoothly XX'
 
 end
 
 section 'features', "Features" do
-	topic 'quickstart',      'Quickstart'
-	topic 'heroku-command',  'Heroku command-line tool'
-	topic 'git',             'Using Git'
-	topic 'sharing',         'Sharing'
-	topic 'console-rake',    'Console and rake'
-	topic 'rack',            'Deploying Rack-based apps'
-	topic 'logs-exceptions', 'Logs and exceptions'
-	topic 'errors',          'Errors'
 	topic 'addons',          'Add-ons'
-	topic 'custom-domains',  'Custom domain names'
-	topic 'gems',            'Installing gems'
-	topic 'taps',            'Database import/export'
-	topic 'renaming-apps',   'Renaming apps'
-	topic 'cron',            'Cron jobs'
 	topic 'background-jobs', 'Background jobs'
 	topic 'config-vars',     'Config vars'
-	topic 'http-caching',    'HTTP caching'
-	topic 'full-text-indexing', 'Full text indexing'
+	topic 'console-rake',    'Console and rake'
 	topic 'constraints',     'Constraints'
-	topic 'technologies',    'Technologies'
+	topic 'custom-domains',  'Custom domain names'
+	topic 'cron',            'Cron jobs'
+  topic '',                'Database XX'
+	topic 'errors',          'Errors'
+	topic 'full-text-indexing', 'Full text indexing'
+	topic 'logs-exceptions', 'Logs and exceptions'
+	topic 'heroku-command',  'Heroku command-line tool'
+	topic 'http-caching',    'HTTP caching'
+	topic 'gems',            'Installing gems'
 end
 
-section 'howto', "How-To's" do
+section 'howto', "How do I:" do
+  topic '', 'send emails XX'
+  topic 'git',             'use Git'
+  topic 'rack',            'deploy Rack-based apps'	
+  topic '', 'use S3 for file uploads XX'
+  topic '', 'backup my app XX'
+  topic 'taps', 'import/export data'
+  topic 'renaming-apps',   'rename my application'	
+	topic 'sharing',         'collaborate with another developer'
 
+end
+
+section 'errors', 'Error Debugging' do
+  topic 'backlog-too-deep', "\"Backlog too deep\""
+  topic '', "Heroku Fail whale"
+  topic '', "General 500"
 end
