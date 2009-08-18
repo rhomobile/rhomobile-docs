2be4435aeef1c56d1197321c9a23c5a1afa53884
Ryan Tomayko <rtomayko@gmail.com>
2009-08-17 19:38:14 -0700

    remove ED annotations from TOC

--- toc.rb
+++ toc.rb
@@ -5,14 +5,14 @@ end
 
 section 'management', "Application Management" do
 	topic 'heroku-command',     'Installing the Heroku command-line tool'
-	topic 'creating-apps',      'Creating apps (RYAN)'
+	topic 'creating-apps',      'Creating apps'
 	topic 'renaming-apps',      'Renaming apps'
 	topic 'git',                'Deploying with Git'
 	topic 'sharing',            'Collaborating with others'
 	topic 'gems',               'Managing Gems'
 	topic 'taps',               'Database import / export'
 	topic 'console',            'Using the console'
-	topic 'rake',               'Running rake tasks (RYAN)'
+	topic 'rake',               'Running rake tasks'
 	topic 'config-vars',        'Config vars'
 	topic 'backups',            'Backups'
 	topic 'addons',             'Managing Addons'
@@ -40,7 +40,6 @@ section 'troubleshooting', "Troubleshooting" do
 	topic 'logs-exceptions',    'Logs and exceptions'
 	topic 'errors',             'Error pages'
 	topic 'performance',        'Performance'
-	topic 'bar',                'fun and games'
 end
 
 # section 'lifecycle', "App Lifecycle" do
