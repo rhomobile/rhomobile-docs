3888cf47fef4438360caceca6b856c67b7b86402
Steven Moore <stave177@gmail.com>
2011-03-15 23:39:37 -0700

    Make css files work for screen AND print, remove debug output, add comments.

--- pdfmaker.rb
+++ pdfmaker.rb
@@ -7,31 +7,30 @@ class PdfMaker
     status, headers, response = @app.call(env)
     
     if env["REQUEST_URI"] =~ /.pdf$/
-      puts "MAKING PDF NOW"
+      # If the page ends in ".pdf", render it as a PDF. Otherwise, don't do anything with it.
       
       content = response.join("")
 
+      # Replace relative links with absolute links
+      # First, regex for all a href elements
       content.gsub!(/<a href="(.*?)">(.*?)<\/a>/) do |match|
-        #match + ' style="font-style:italic"'
         url = $1
         text = $2
         
+        # If there's no colon in the URL, then assume it's a relative link, and make it absolute
         if !(url.include? ":")
-          puts "No colon link: " + match
-          puts "url is #{url} and text is #{text}"
-          
           if !( url =~ /^\// )
             url = "/"+url
           end
           
           '<a href="http://'+ env["HTTP_HOST"] + url.to_s + '" style="font-style:italic">' + text.to_s + '</a>'
         else
-          puts "Colon link: " + match
+          # Colon in the URL, leave it as is (ftp, http, mailto...)
           match
         end
       end
 
-    
+      # Create a PDFKit object, apply some CSS to it
       kit = PDFKit.new(content)
 
       kit.stylesheets << File.join( File.dirname(__FILE__), 'public', 'stylesheets', 'pdf.css').to_s
@@ -41,8 +40,9 @@ class PdfMaker
       body = kit.to_pdf
 
       response = [body]
-      headers["Content-Length"] = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
+
       headers["Content-Type"] = "application/pdf"
+      headers["Content-Length"] = (body.respond_to?(:bytesize) ? body.bytesize : body.size).to_s
     end
     
     [status, headers, response]
