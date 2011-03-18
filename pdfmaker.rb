c97dd49d017479591bf4d43e9b325a8a99fddf30
Steven Moore <stave177@gmail.com>
2011-03-17 18:46:26 -0700

    Backing out last changes - locally hosted images deadlock a single Dyno on heroku

--- pdfmaker.rb
+++ pdfmaker.rb
@@ -15,25 +15,32 @@ class PdfMaker
 
       # Replace note elements with actual note icons, since PDFKit won't render background images as
       # defined in external CSS files
-      content.gsub!(/<td class="icon"><\/td>/) do |match|
-        "<td class=\"icon\"><img src=\"/images/note.png\"></td>"
-      end
+      # Commented out because we run on a single thread on Heroku. Trying to load a local image
+      # while we're rendering a PDF will deadlock
+      #content.gsub!(/<td class="icon"><\/td>/) do |match|
+      #  #puts "Subbing for note icon"
+      #  "<td class=\"icon\"><img src=\"/images/note.png\"></td>"
+      #end
 
       # Use regexes to replace relative links/images with fully qualified links/images
       # Otherwise, PDF will try to link to file:// and images will be broken
-      content.gsub!(/<(a href|img src)="(.*?)">(.*?)(?:<\/a>|)/) do |match|
+
+      # Don't use the one for images. Same issue as with icons, trying to load a locally hosted
+      # image will deadlock.
+      #content.gsub!(/<(a href|img src)="(.*?)">(.*?)(?:<\/a>|)/) do |match|
+      content.gsub!(/<(a href)="(.*?)">(.*?)(?:<\/a>|)/) do |match|
         type = $1
         url = $2
         text = $3
         
         # If there's no colon in the URL, then assume it's a relative link, and make it absolute
         if !(url.include? ":")
-          #puts "Subbing for " + type + " " + url
+          # puts "Subbing for " + type + " " + url
           if !( url =~ /^\// )
             url = "/"+url
           end
           
-          '<'+type+'="http://'+ env["HTTP_HOST"] + url.to_s + '" style="font-style:italic">' + text.to_s + '</a>'
+          '<'+type+'="http://'+ env["HTTP_HOST"] + url.to_s + '">' + text.to_s + '</a>'
         else
           # Colon in the URL, leave it as is (ftp, http, mailto...)
           match
