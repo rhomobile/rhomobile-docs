079812918d35acfaa85159672b79339181009914
larsburgess <larsburgess@gmail.com>
2011-12-22 11:24:28 -0800

    using same prefix for section titles

--- toc.rb
+++ toc.rb
@@ -10,7 +10,7 @@ section 'getting-started', "Getting Started" do
   topic 'secure-apps',          'Building Highly Secure Apps'
 end
 
-section 'rhodes-reference', "Rhodes Developer Reference" do
+section 'rhodes-reference', "Rhodes: Developer Reference" do
   topic 'rhodes/introduction',      'Introduction'
   topic 'rhodes/install',           'Install Rhodes'
   topic 'rhodes/generator',         'App Generator'
@@ -30,14 +30,14 @@ section 'rhodes-reference', "Rhodes Developer Reference" do
   topic 'credits',                  'Credits'
 end
 
-section 'rhoconnect-reference', "RhoConnect Getting Started" do
+section 'rhoconnect-reference', "RhoConnect: Getting Started" do
   topic 'rhoconnect/introduction',      'Introduction'
   topic 'rhoconnect/contributing',      'Contributing'
   topic 'rhoconnect/install',           'Installation'
   topic 'rhoconnect/command-line',      'App Generator and Rake Tasks'
 end
 
-section 'rhoconnect-source-adapter-reference', "RhoConnect Source Adapters" do
+section 'rhoconnect-source-adapter-reference', "RhoConnect: Source Adapters" do
   topic 'rhoconnect/source-adapters',   'Source Adapters'
   topic 'rhoconnect/authentication',    'Adding Device Authentication'
   topic 'rhoconnect/testing',           'Testing and Debugging'
@@ -47,14 +47,14 @@ section 'rhoconnect-source-adapter-reference', "RhoConnect Source Adapters" do
   topic 'rhoconnect/metadata',          'Dynamic Views with Metadata'
 end
 
-section 'rhoconnect-plugin-reference', "RhoConnect Plugins" do
+section 'rhoconnect-plugin-reference', "RhoConnect: Plugins" do
   topic 'rhoconnect/plugin-intro',      'Introduction'
   topic 'rhoconnect/rails-plugin',      'Rails Plugin'
   topic 'rhoconnect/net-plugin',        '.Net Plugin'
   topic 'rhoconnect/java-plugin',       'Java Plugin'
 end
 
-section 'rhoconnect-administration', "RhoConnect Administration" do
+section 'rhoconnect-administration', "RhoConnect: Administration" do
   topic 'rhoconnect/settings',          'RhoConnect Settings'
   topic 'rhoconnect/web-console',       'Admin Web Interface'
   topic 'rhoconnect/licensing',         'Licensing your App'
@@ -63,35 +63,35 @@ section 'rhoconnect-administration', "RhoConnect Administration" do
   topic 'rhoconnect/stats-middleware',  'Collecting Runtime Stats'
 end
 
-section 'rhoconnect-deployment', "RhoConnect Deployment" do
+section 'rhoconnect-deployment', "RhoConnect: Deployment" do
   topic 'rhoconnect/deploying',         'Deployment and Hosting Options'
   topic 'rhoconnect/rhoconnect-redis-stack',  'Creating a RhoConnect Stack on AWS'
 end
 
-section 'rhoconnect-advanced', "RhoConnect Advanced Topics" do
+section 'rhoconnect-advanced', "RhoConnect: Advanced Topics" do
   topic 'rhoconnect/bulk-sync',         'Big Datasets and Bulk Sync'
   topic 'rhoconnect/rest-api',          'RhoConnect REST API'
   topic 'rhoconnect/extending-rhoconnect-server', 'Extending Rhoconnect App with custom routes'
   topic 'rhoconnect/migration',         'Migrating from RhoSync to RhoConnect'
 end
 
-section 'rhoconnect-nonrhodes-client-reference', "RhoConnect non-Rhodes Client Developer Reference" do
+section 'rhoconnect-nonrhodes-client-reference', "RhoConnect: non-Rhodes Client Developer Reference" do
   topic 'rhoconnect/client',            'Using RhoConnect without Rhodes'
   topic 'rhoconnect/client-objc',       'RhoConnect without Rhodes: Objective C'
 end
 
-section 'rhoconnectjs-reference', "RhoConnect JS Client Developer Reference" do
+section 'rhoconnectjs-reference', "RhoConnect: JS Client Developer Reference" do
   topic 'rhoconnectjs/introduction',      'Introduction'
   topic 'rhoconnectjs/getting-started',   'Getting Started'
   topic 'rhoconnectjs/reference',         'API Reference'
   topic 'rhoconnectjs/creating-app',      'Creating an App'
 end
 
-section 'rhoconnect-service-reference', "RhoConnect Services" do
+section 'rhoconnect-service-reference', "RhoConnect: Hosted Services" do
   topic 'rhoconnect/heroku-addon',      'Heroku Addon'
 end
 
-section 'rhoconnect-adapters-reference', "RhoConnect Adapters" do
+section 'rhoconnect-adapters-reference', "RhoConnect: Vendor Adapters" do
   topic 'rhoconnect/adapters-intro',    'Introduction'
   topic 'rhoconnect/adapters-crm',      'CRM adapters'
 end
