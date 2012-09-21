ab678f378ef0c13b9e2424609a73ec6f7a7ae929
David Strom <david@rhomobile.com>
2012-09-20 18:40:54 -0700

    fixed a broken link on the rhostudio - rhohub tutorial put the rhomobile install instructions into only one place int he docs and updated the links

--- toc.rb
+++ toc.rb
@@ -2,6 +2,7 @@ section 'intro', 'Introduction'
 
 section 'getting-started', 'Getting Started' do
   topic 'home',                 'Home'
+  topic 'rhomobile-install',   'RhoMobile Suite Install'
   topic 'rhoelements/rhoelements-tutorial',   'RhoElements Tutorial'
   topic 'rhostudio.tutorial',   'RhoStudio Tutorial'
   topic 'rhoconnect/tutorial',  'RhoConnect Tutorial'
