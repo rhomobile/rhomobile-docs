d1ccaae11b779d8f0a72e0a3fbedc60a4da878cf
cfh463 <rob.galvin@motorolasolutions.com>
2012-11-16 14:29:48 -0500

    api matrix update

--- indicators.rb
+++ indicators.rb
@@ -315,7 +315,7 @@ The following table shows the compatibility for each Mobile API.
    </tbody></table>"
 
     md = md + '
-##API Matrix (MSI Devices)&nbsp;<img src="../img/motowebkit.png"/>
+##API Matrix (MSI Devices)
 <table class="table table-striped table-bordered table-condensed apicompat" width="130%" id="matrixmsi">
 <thead>
   <tr>
