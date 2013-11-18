6b85af0bd0fa06b5f6578c1bec6b64128e4ad339
cfh463 <rob.galvin@motorolasolutions.com>
2013-11-18 09:09:14 -0500

    [#60948610] added ability to define code tutorials with tie in to Git project

---
+++ tuts.rb
@@ -0,0 +1,13 @@
+
+tutorial 'emberjs', 'Using EmberJS in RhoMobile Apps','Javascript MVC','https://github.com/rhomobile/rhotut-emberjs' do
+  gitlabel 'step-0',                 'This is Step 0'
+  gitlabel 'step-1',                 'This is Step 1'
+  gitlabel 'step-2',                 'This is Step 2'
+
+end
+tutorial 'angularjs', 'Using AngularJS in RhoMobile Apps','Javascript MVC','https://github.com/rhomobile/rhotut-angularjs' do
+  gitlabel 'step-0',                 'This is Step 0'
+  gitlabel 'step-1',                 'This is Step 1'
+  gitlabel 'step-2',                 'This is Step 2'
+
+end
