723d4fc1bcf6fc8c95ca5d0dccde8763ff40ee79
Michael Toews <michael.toews@motorolasolutions.com>
2013-12-04 11:17:00 -0800

    Create folder structure of new format

---
+++ docs/English/edge/toc.rb
@@ -0,0 +1,282 @@
+
+section 'getting-started', 'Introduction','Mobile Guides' do
+  topic 'guide/welcome',                 'Welcome to RhoMobile Suite'
+  topic 'guide/rhoArchitecture',   'Supported Platforms'
+  topic 'guide/devskills',   'Developer Skills'
+  topic 'guide/devenvironment',  'Developer Environments'
+  topic 'guide/appdesign',   'App Architecture'
+  topic 'guide/uichoices',      'UI Architecture'
+  topic 'guide/kitchensink',                 'KitchenSink Application'
+  topic 'guide/apiusage',      '2.2 Migration Guide'
+  topic 'guide/api_js', 'RhoMobile Javascript Usage'
+  topic 'guide/api_ruby', 'RhoMobile Ruby Usage'
+  topic 'guide/licensing',      'Licensing'
+end
+
+
+
+
+section 'rhodescommonapi-reference', '4.0 API Reference','Mobile API Docs' do
+  topic 'guide/apisummary', 'RhoMobile API Summary'
+  
+  topic 'api/Application',      'Application'
+  topic 'api/barcode',      'Barcode'
+  topic 'api/battery',      'Battery'
+  topic 'api/cardreader',      'CardReader'
+  topic 'api/Database',      'Database '
+  topic 'api/keycapture',      'KeyCapture '
+  topic 'api/keystate',      'KeyState '
+  topic 'api/Log',      'Log'
+  topic 'api/mediaplayer',      'Mediaplayer '
+  topic 'api/NativeMenubar',      'NativeMenubar '
+  topic 'api/NativeTabbar',      'NativeTabbar '
+  topic 'api/NativeToolbar',      'NativeToolbar '
+  topic 'api/Navbar',      'Navbar '
+  topic 'api/Network',      'Network'
+  topic 'api/Notification',      'Notification'
+  topic 'api/Orm',      'ORM Javascript'
+  topic 'api/rhom-api',      'ORM Ruby'
+  topic 'api/OrmModel',      'ORM Model Javascript'
+  topic 'api/push',      'Push'
+  topic 'api/RhoConnectClient',      'RhoConnectClient'
+  topic 'api/File',      'RhoFile '
+  topic 'api/screenorientation',      'ScreenOrientation'
+  topic 'api/sensor',      'Sensor'
+  topic 'api/signalindicators',      'SignalIndicators'
+  topic 'api/system',      'System'
+  topic 'api/webview',      'WebView'
+end
+
+section 'rhodescommonapi-22reference', '2.x API Reference','Mobile API Docs' do
+  topic 'v/2.2/rhoelements/alarm',      'Alarm'
+  topic 'v/2.2/rhoelements/apd',      'Apd'
+  topic 'v/2.2/rhoelements/AudioCapture',      'AudioCapture'
+  topic 'v/2.2/rhodesapi/bluetoothmanager-api',             'BluetoothManager'
+  topic 'v/2.2/rhodesapi/bluetoothsession-api',             'BluetoothSession'
+  topic 'v/2.2/rhodesapi/camera-api',             'Camera'
+  topic 'v/2.2/rhoelements/comm','Comm'
+  topic 'v/2.2/rhodesapi/geolocation-api',             'GeoLocation'
+  topic 'v/2.2/rhoelements/Gesture',      'Gesture'
+  topic 'v/2.2/rhoelements/hourglass',      'Hourglass'
+  topic 'v/2.2/rhoelements/Keylight',      'Key Light'
+  topic 'v/2.2/rhodesapi/mapview-api',             'MapView'
+  topic 'v/2.2/rhoelements/memory',             'Memory'
+  topic 'v/2.2/rhodesapi/NdefMessage-api',             'NdefMessage'
+  topic 'v/2.2/rhodesapi/NdefRecord-api',             'NdefRecord'
+  topic 'v/2.2/rhodesapi/NFCManager-api',             'NFCManager'
+  topic 'v/2.2/rhodesapi/NFCTag-api',             'NFCTag'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_IsoDep-api',             'NFCTagTechnology_IsoDep'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_MifareClassic-api',      'NFCTagTechnology_MifareClassic'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_MifareUltralight-api',      'NFCTagTechnology_MifareUltralight'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_Ndef-api',      'NFCTagTechnology_Ndef'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_NdefFormatable-api',      'NFCTagTechnology_NdefFormatable'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_NfcA-api',      'NFCTagTechnology_NfcA'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_NfcB-api',      'NFCTagTechnology_NfcB'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_NfcF-api',      'NFCTagTechnology_NfcF'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology_NfcV-api',      'NFCTagTechnology_NfcV'
+  topic 'v/2.2/rhodesapi/NFCTagTechnology-api',      'NFCTagTechnology'
+  topic 'v/2.2/rhoelements/registry',      'Registry'
+  topic 'v/2.2/rhoelements/reboot',      'Reboot'
+  topic 'v/2.2/rhoelements/rfid',      'Rfid'
+  topic 'v/2.2/rhodesapi/rhoconfig-api',             'RhoConfig'
+  topic 'v/2.2/rhodesapi/rhocontact-api',             'RhoContact'
+  topic 'v/2.2/rhodesapi/rhocontroller-api',             'RhoController'
+  topic 'v/2.2/rhodesapi/rhoerror-api',             'RhoError'
+  topic 'v/2.2/rhodesapi/rhoevent-api',             'RhoEvent'
+  topic 'v/2.2/rhodesapi/rhoprofiler-api',             'RhoProfiler'
+  topic 'v/2.2/rhodesapi/rhomsource-api',             'RhomSource'
+  topic 'v/2.2/rhodesapi/rhoutils-api',             'RhoUtils'
+  topic 'v/2.2/rhoelements/SignatureCapture',      'SignatureCapture'
+  topic 'v/2.2/rhodesapi/signaturecapture-api',             'SignatureCapture (Inline)'
+  topic 'v/2.2/rhoelements/stylus',      'Stylus'
+  topic 'v/2.2/rhoelements/systemTime',      'SystemTime'
+  topic 'v/2.2/rhodesapi/timer-api',             'Timer API'
+  topic 'v/2.2/rhoelements/timer',      'Timer'
+  topic 'v/2.2/rhoelements/VideoCapture',             'VideoCapture'
+  topic 'v/2.2/rhoelements/volume',      'Volume'
+  topic 'v/2.2/rhoelements/wake',             'Wake'
+end
+
+
+section 'rhomob-setup', 'Dev Environment Setup','Mobile Guides' do
+  topic 'guide/rhomobile-install',   'RhoMobile Suite Install'
+  topic 'guide/nativesdksetup',             'Native SDK Setup'
+  topic 'guide/nonrhostudio',             'Developing Without RhoStudio'
+  topic 'guide/visualstudio',             'Developing with Visual Studio'
+  topic 'guide/rhohubtutorial',             'Build In The Cloud With RhoHub'
+  topic 'rhostudio-rhohub',      'Using RhoStudio with RhoHub'
+  
+end
+
+section 'rhomob-apps', 'Developing Apps','Mobile Guides' do
+  topic 'guide/creating_a_project',         'Starting a New Project'
+  topic 'guide/runtime_config',             'Runtime Configuration'
+  topic 'guide/build_config',             'Buildtime Configuration'
+  topic 'guide/build_ios',             'Build for iOS'
+  topic 'guide/build_android',             'Build for Android'
+  topic 'guide/build_wm',             'Build for Windows Mobile'
+  topic 'guide/build_wp8',             'Build for Windows Phone 8'
+  topic 'guide/build_win',             'Build for Windows'
+  topic 'guide/using_emulators',             'Using Native Emulators'
+  topic 'guide/app_icon_splash',        'Application Icon & Splash Screen'
+  topic 'guide/css-framework',        'CSS Frameworks'
+  topic 'guide/webapps',        'RhoMobile for Web Apps'
+  topic 'guide/rhoelements-runtime',        'RhoElements Shared Runtime'
+  topic 'guide/webcontainer', 'Custom Web App Container'
+  topic 'guide/app_upgrade', 'Upgrading Your App'
+end
+
+section 'rhomob-data', 'Data Handling','Mobile Guides' do
+  topic 'guide/local_database',              'Local Database and Rhom'
+  topic 'guide/rhom_ruby',              'Using Rhom with Ruby'
+  topic 'guide/rhom_js',              'Using Rhom with JavaScript'
+  topic 'guide/rhom_backbone',              'Using Rhom with Backbone.JS'
+  topic 'guide/rhom_angular',   'Using Rhom with Angular JS'
+  topic 'guide/using_rhoconnect',   'Synchronization'
+  topic 'guide/web_services', 'Web Services & AJAX'
+end
+
+section 'rhomob-dev', 'Device Capabilities','Mobile Guides' do
+  topic 'guide/barcode',       'Barcode'
+  topic 'guide/bluetooth',       'Bluetooth'
+  topic 'guide/camera',       'Camera'
+  topic 'guide/pim',       'Contacts & Calendar'
+  topic 'guide/controlling_webview_behavior',       'Controlling Webview Behavior'
+  topic 'guide/device_hardware_key_handling',       'Device Notifications & Keylight'
+  topic 'guide/device_information_and_abilities',       'Detecting Device Abilities'
+  topic 'guide/hardware_accleration',       'Hardware Acceleration'
+  topic 'guide/html5',       'Html5 Tags'
+  topic 'guide/handling_local_files',       'File System'
+  topic 'guide/geolocation',       'Geolocation'
+  topic 'guide/localization',       'Localization'
+  topic 'guide/maps',       'Mapping'
+  topic 'guide/media',       'Media & Ringtones'
+  topic 'guide/native_ui_elements', 'Native UI Components'
+  topic 'guide/interfacing_with_the_network', 'Network Interfacing'
+  topic 'v/2.2/rhodes/nfc',       'NFC'
+  topic 'guide/screen_orientation', 'Orientation and Responsive Design'
+  topic 'guide/printing',       'Printing'
+  topic 'guide/push_notifications',       'Push Notifications'
+  topic 'guide/device_sensors',       'Sensors'
+  topic 'guide/signature',       'Signature'
+  topic 'guide/urlschemes', 'Url Schemes'
+
+end
+
+section 'rhomob-test', 'Testing & Debugging','Mobile Guides' do
+  topic 'guide/debugging_with_rhosimulator',    'Debugging with RhoSimulator'
+  topic 'guide/remote-debug',    'Debugging JS On Devices'
+  topic 'guide/logging',    'Logging'
+  topic 'guide/testing',    'Testing'
+  topic 'guide/profiling',    'Performance Profiling'
+end
+
+
+
+section 'rhomob-ext', 'Extending','Mobile Guides' do
+  topic 'guide/native_extensions',        'Extending the Rhodes Framework'
+  topic 'guide/ruby_extensions',        'Ruby Extensions'
+  topic 'guide/view_extensions',        'View Extensions'
+end
+
+
+section 'rhoconnect-reference', 'Getting Started','RhoConnect Guides' do
+  topic 'rhoconnect/introduction',      'Introduction'
+  topic 'rhoconnect/install',           'Installation'
+  topic 'rhoconnect/migration',     'Migrating to RhoConnect 4.0'
+  topic 'rhoconnect/command-line',      'App Generator and Command Line'
+  topic 'rhoconnect/supported-platforms', 'Supported Platforms'
+  topic 'rhoconnect/rhoconnect-calculator', 'RhoConnect Calculator'
+end
+
+section 'rhoconnect-source-adapter-reference', 'Source Adapters','RhoConnect Guides' do
+  topic 'rhoconnect/source-adapters-intro', 'Introduction'
+  topic 'rhoconnect/source-adapters',   'Source Adapters (Ruby)'
+  topic 'rhoconnect/source-adapters-js',   'Source Adapters (JavaScript)'
+  topic 'rhoconnect/authentication',    'Adding Device Authentication'
+  topic 'rhoconnect/testing',           'Testing and Debugging'
+  topic 'rhoconnect/blob-sync',         'Binary Objects and Blob Sync'
+  topic 'rhoconnect/async-jobs',        'Adapter Jobs with Resque'
+  topic 'rhoconnect/metadata',          'Dynamic Views with Metadata'
+  topic 'rhoconnect/data-partitioning',     'Data Partitioning & Pass-Through'
+end
+
+section 'rhoconnect-ruby-api-reference', 'Source Adapter Ruby APIs','RhoConnect Guides' do
+  topic 'rhoconnect/rest-api',          'RhoConnect REST API'
+  topic 'rhoconnectapi/source-adapter-controller-api-ruby', 'Source Adapter Controller API'
+  topic 'rhoconnectapi/source-adapter-model-api-ruby', 'Source Adapter Model API'
+  topic 'rhoconnectapi/test-api-ruby',     'Source Adapter Test API'
+end
+
+section 'rhoconnect-js-api-reference', 'Source Adapter JS APIs','RhoConnect Guides' do
+  topic 'rhoconnect/rest-api',          'RhoConnect REST API'
+  topic 'rhoconnectapi/source-adapter-controller-api-js', 'Source Adapter Controller API'
+  topic 'rhoconnectapi/source-adapter-model-api-js', 'Source Adapter Model API'
+  topic 'rhoconnectapi/source-adapter-request-api-js', 'Source Request API'
+  topic 'rhoconnectapi/source-adapter-response-api-js', 'Source Response API'
+  topic 'rhoconnectapi/source-adapter-store-api-js', 'Source Adapter Store API'
+  topic 'rhoconnectapi/source-adapter-exception-api-js', 'Source Adapter Exception API'
+end
+
+section 'rhoconnect-plugin-reference', 'Plugins','RhoConnect Guides' do
+  topic 'rhoconnect/plugin-intro',      'Introduction'
+  topic 'rhoconnect/rails-plugin',      'Rails Plugin'
+  topic 'rhoconnect/net-plugin',        '.Net Plugin'
+  topic 'rhoconnect/java-plugin',       'Java Plugin'
+end
+
+section 'rhoconnect-push-reference', 'Push Sync','RhoConnect Guides' do
+  topic 'rhoconnect/push',      'Introduction to Push'
+  topic 'rhoconnect/push-client-setup-rps',       'Setup for RhoConnect Push Service'
+  topic 'rhoconnect/push-client-setup-android',       'Setup for Android'
+  # topic 'rhoconnect/push-client-setup-bb',       'Setup for BlackBerry'
+  topic 'rhoconnect/push-client-setup-ios',       'Setup for iOS'
+  topic 'rhoconnect/push-client-setup',       'Finishing Setup for all Clients'
+  topic 'rhoconnect/push-backend-setup',        'Setup on Backend'
+  topic 'rhoconnect/push-testing',      'Testing Push'
+end
+
+section 'rhoconnect-administration', 'Administration','RhoConnect Guides' do
+  topic 'rhoconnect/settings',          'RhoConnect Settings'
+  topic 'rhoconnect/web-console',       'Admin Web Interface'
+  topic 'rhoconnect/licensing',         'Licensing your App'
+  topic 'rhoconnect/benchmarks',        'Rhoconnect Benchmarking'
+  topic 'rhoconnect/benchmarks-running','Running Rhoconnect Benchmarks'
+  topic 'rhoconnect/stats-middleware',  'Collecting Runtime Stats'
+end
+
+section 'rhoconnect-deployment', 'Deployment','RhoConnect Guides' do
+  topic 'rhoconnect/preparing-production',         'Preparing for Production'
+  topic 'rhoconnect/hosting-rhohub',         'Hosting on RhoHub'
+  topic 'rhoconnect/deploying',         'Deploying RhoConnect'
+  topic 'rhoconnect/rhoconnect-redis-stack',  'High Availability Deployment on AWS'
+end
+
+section 'rhoconnect-advanced', 'Advanced Topics','RhoConnect Guides' do
+  topic 'rhoconnect/bulk-sync',         'Big Datasets and Bulk Sync'
+  topic 'rhoconnect/extending-rhoconnect-server', 'Extending Rhoconnect App with custom routes'
+  topic 'rhoconnect/cud-conflicts',     'Resolving Conflicts'
+end
+
+section 'rhoconnect-nonrhodes-client-reference', 'Clients','RhoConnect Guides' do
+  topic 'rhoconnect/client',            'Using RhoConnect without Rhodes'
+  topic 'rhoconnect/client-objc',       'RhoConnect Client for iOS'
+  topic 'rhoconnect/client-java',       'RhoConnect without Rhodes: Android Java'
+  topic 'rhoconnectjs/introduction',      'RhoConnect.JS Client'
+  topic 'rhoconnectjs/getting-started',   'RhoConnect.JS Getting Started'
+  topic 'rhoconnectjs/reference',         'RhoConnect.JS API Reference'
+  topic 'rhoconnectjs/creating-app',      'RhoConnect.JS Creating an App'
+end
+
+
+section 'rhoconnect-service-reference', 'Hosted Services','RhoConnect Guides' do
+  topic 'rhoconnect/heroku-addon',      'Heroku Addon'
+end
+
+section 'rhoconnect-adapters-reference', 'Vendor Adapters','RhoConnect Guides' do
+  topic 'rhoconnect/adapters-intro',    'Introduction'
+  topic 'rhoconnect/adapters-crm',      'CRM adapters'
+end
+
+
+
