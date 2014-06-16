629c64e0e6ca2b714b77e0c8ceae165f625bc599
Michael Toews <michael.toews@motorolasolutions.com>
2014-06-16 10:11:59 -0700

    Work on hosted services separation.

---
+++ docs/en/hosted/guide/welcome.md
@@ -0,0 +1,1 @@
+# Welcome to RhoMobile Hosted Services
---
+++ docs/en/hosted/toc.rb
@@ -0,0 +1,8 @@
+section 'getting-started',                  'Introduction','Hosted Services' do
+  topic 'en/hosted/guide/introduction',     'Rhomobile Hosted Services Introduction'
+end
+
+section 'Guides',                            'Guides','Guides' do
+  topic 'en/hosted/guide/rhohubtutorial',    'Remote Build Guide'
+  topic 'en/hosted/guide/rhostudio-rhohub',  'Building Remotely through RhoStudio'
+end
