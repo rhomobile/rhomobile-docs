8b13b02c50b311a49925b82facbe41171e6e4ea5
cfh463 <rob.galvin@motorolasolutions.com>
2015-01-20 17:07:39 -0500

    [#86448980] updated matrix data with dummy api

--- eb/en/edge/export/offline/offlineDocs/docs/matrix.js
+++ eb/en/edge/export/offline/offlineDocs/docs/matrix.js
@@ -1,33 +1,427 @@
 var matrix=[
     {
-        "api": "Addressbar",
-        "engines": "Webkit,IE",
+        "api":"AddressBar", 
+       "engines": "Webkit,IE",
         "os": "Android,WM,CE",
-        "devices": "MC40,TC55,TC75"
-    },
-    {
-        "api": "Alarm",
-        "engines": "Webkit",
-        "os": "WM,CE",
-        "devices": "TC75"
-    },
-    {
-        "api": "Application",
-        "engines": "IE",
-        "os": "Android,WM,CE",
-        "devices": "MC40,TC55,TC75"
-    },
-    {
-        "api": "Barcode",
-        "engines": "Webkit,IE",
-        "os": "Android",
-        "devices": "MC30"
-    },
-    {
-        "api": "BlahBlah",
-        "engines": "Webkit,IE",
-        "os": "Android,WM,CE",
-        "devices": "MC40,TC55,TC75"
+        "devices": "All"
     },
 
+    {
+        "api":"AirBeam",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Alarm",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Application",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"AudioCapture",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"BackButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Backlight",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Barcode",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+    {
+        "api":"Battery",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"BottomCommandArea",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"CardReader",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Comm",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Device",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"File",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"FileTransfer",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"ForwardButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Gesture",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"GoButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"HomeButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Hourglass",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Imager",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Intent",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"KeyCapture",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"KeyState",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"KeyLight",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Log",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Mediaplayer",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Memory",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"NativeMenubar",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"NativeTabbar",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"NativeToolbar",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Network",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Notification",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"PowerOn",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Printer",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"PrinterZebra",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Process",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Push",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"QuitButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Reboot",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Registry",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"ReloadButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"ScreenOrientation",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Sensor",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"SignalIndicators",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Signature",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Sip",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"SipButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"SmartCradle",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"StopButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Stylus",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"System",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"SystemTime",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"TopCommandArea",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Videocapture",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Volume",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Wake",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Websocket",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"WebView",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"Zoom",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
+
+    {
+        "api":"ZoomTextButton",
+        "engines": "Webkit,IE",
+        "os": "Android,WM,CE",
+        "devices": "All"
+    },
 ];
